36b94642e393e6cb1e86847a91d96d6894e03dad
psuchta <pawel.suchta@polcode.net>
2022-08-14 19:00:26 +0200

    Add tests

--- tests/test_genetic_helper.py
+++ tests/test_genetic_helper.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 from genetic_helper import GeneticHelper
+from genome_helper import GenomeHelper
 
 class TestGeneticHelper(unittest.TestCase):
   def setUp(self):
@@ -10,6 +11,11 @@ class TestGeneticHelper(unittest.TestCase):
     with patch('random.randint', return_value=3) as mock_random:
       result = self.genetic_helper.crossover([0, 1, 0, 1, 1, 0, 1],[1, 1, 0, 1, 0, 1, 0])
       self.assertEqual(([0, 1, 0, 1, 0, 1, 0], [1, 1, 0, 1, 1, 0, 1] ), result)
+
+  def test_crossover_ieee_754(self):
+    with patch('random.randint', return_value=4) as mock_random:
+      result = self.genetic_helper.crossover_ieee_754([1,0,1,1,0,1,0,0,0,0,0,0,0,0,1,1],[0,1,1,0,1,0,0,1,1,0,1,0,1,0,0,0])
+      self.assertEqual(([1,0,1,1,1,0,0,1,1,0,1,0,1,0,0,0], [0,1,1,0,0,1,0,0,0,0,0,0,0,0,1,1] ), result)
 
   def test_crossover_with_incompatibile_arrays(self):
     with self.assertRaisesRegex(Exception, "Lengths of passed arrays are not the same"):
@@ -21,10 +27,16 @@ class TestGeneticHelper(unittest.TestCase):
 
     self.assertEqual([1,1,1,0,0,1,0,1,1,0,1,1], binary_genome)
 
+  def test_mutate_ieee_754_genome_with_high_probability(self):
+    binary_genome = [1,0,1,1,0,1,0,0,0,0,0,0,0,0,1,1, 1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,1]
+    self.genetic_helper.mutate_ieee_754_genome(binary_genome, probability=1.0)
+
+    self.assertEqual([0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,0,  0,0,0,0,0,0,0,1,1,0,1,1,1,1,0,0], binary_genome)
+
   def test_create_random_generation(self):
-    result = self.genetic_helper.create_random_generation(5, 6)
+    result = self.genetic_helper.create_random_generation(5, 2)
     self.assertEqual(5, len(result))
-    self.assertEqual(6, len(result[0]))
+    self.assertEqual(2 * GenomeHelper.GENES_PER_NUMBER , len(result[0]))
 
 
 if __name__ == '__main__':
