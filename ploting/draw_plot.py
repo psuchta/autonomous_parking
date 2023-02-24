e7b5629e17174e061cd4f32d61ad87e17a7b4216
Pawel Suchta <pawel.suchta@polcode.net>
2023-02-24 02:50:50 +0100

    Add custom colors for plots

--- ploting/draw_plot.py
+++ ploting/draw_plot.py
@@ -8,13 +8,13 @@ def draw_one_plot(source):
 
   diagram = df.plot(x="Generation", y=["Best fitness", "Population fitness mean", "Top10 fitness mean"], ylabel="Fitness")
   # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
-  plt.axhline(y = 1.3, color = 'r', linestyle = '--'),
+  plt.axhline(y=1.3, color='r', linestyle='--'),
   # Fitness value above 1.7 means the car parked ideally
-  plt.axhline(y = 1.7, color = 'b', linestyle = '--')
+  plt.axhline(y=1.7, color='b', linestyle='--')
   plt.legend(["Best", "Population mean", "Top10 mean", "Parked well", "Parked perfectly"], prop={'size': 8})
   plt.show()
 
-def draw_multiple(sources, labels):
+def draw_multiple(sources, labels, colors=None):
   dfs = []
   for source in sources:
     df = pd.read_csv(source)
@@ -24,12 +24,12 @@ def draw_multiple(sources, labels):
   for i in range(1, len(dfs)):
     merged_df = merged_df.merge(dfs[i],on='Generation')
 
-  diagram = merged_df.plot(x="Generation", ylabel="Fitness")
+  diagram = merged_df.plot(x="Generation", ylabel="Fitness", color=colors)
 
   # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
-  plt.axhline(y = 1.3, color = 'r', linestyle = '--')
+  plt.axhline(y=1.3, color='r', linestyle='--')
   # Fitness value above 1.7 means the car parked ideally
-  plt.axhline(y = 1.7, color = 'b', linestyle = '--')
+  plt.axhline(y=1.7, color='cyan', linestyle='--')
 
   # plt.legend(labels + ["Parked well", "Parked perfectly"], prop={'size': 8}, loc='lower right')
   plt.legend(labels + ["Parked well", "Parked perfectly"], prop={'size': 8})
@@ -47,5 +47,7 @@ labels = [
   "Label for path2"
 ]
 
+colors =['blue', 'green', '#A2142F']
+
 # draw_one_plot('path')
-draw_multiple(sources, labels)
+draw_multiple(sources, labels, colors)
