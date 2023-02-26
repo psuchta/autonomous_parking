f8e56adf89a4ffc34634e93a29495520c767a012
Pawel Suchta <pawel.suchta@polcode.net>
2023-02-26 19:24:01 +0100

    Update plot drawing

--- ploting/draw_plot.py
+++ ploting/draw_plot.py
@@ -3,15 +3,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def draw_one_plot(source):
+def draw_one_plot(source, colors=None):
   df = pd.read_csv(source)
 
-  diagram = df.plot(x="Generation", y=["Best fitness", "Population fitness mean", "Top10 fitness mean"], ylabel="Fitness")
-  # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
-  plt.axhline(y=1.3, color='r', linestyle='--'),
+  diagram = df.plot(
+    x="Generation",
+    y=["Best fitness", "Population fitness mean", "Top10 fitness mean"],
+    ylabel="Fitness",
+    color=colors
+  )
   # Fitness value above 1.7 means the car parked ideally
-  plt.axhline(y=1.7, color='b', linestyle='--')
-  plt.legend(["Best", "Population mean", "Top10 mean", "Parked well", "Parked perfectly"], prop={'size': 8})
+  plt.axhline(y=1.7, color='black', linestyle='--')
+  # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
+  plt.axhline(y=1.3, color='blue', linestyle='--'),
+  plt.legend(["Best", "Population mean", "Top10 mean", "Parked perfectly", "Parked well"], prop={'size': 9})
   plt.show()
 
 def draw_multiple(sources, labels, colors=None):
@@ -26,28 +31,25 @@ def draw_multiple(sources, labels, colors=None):
 
   diagram = merged_df.plot(x="Generation", ylabel="Fitness", color=colors)
 
-  # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
-  plt.axhline(y=1.3, color='r', linestyle='--')
   # Fitness value above 1.7 means the car parked ideally
-  plt.axhline(y=1.7, color='cyan', linestyle='--')
+  plt.axhline(y=1.7, color='black', linestyle='--')
+  # 1.3 fitness value indicates that the car parked in the specified space imprecisely(but good enough)
+  plt.axhline(y=1.3, color='blue', linestyle='--')
 
-  # plt.legend(labels + ["Parked well", "Parked perfectly"], prop={'size': 8}, loc='lower right')
-  plt.legend(labels + ["Parked well", "Parked perfectly"], prop={'size': 8})
+  plt.legend(labels + ["Parked perfectly", "Parked well"], prop={'size': 9})
   plt.show()
 
 # Put here paths to csv files
 sources=[
-  'path1',
-  'path2'
+  '',
+  '',
 ]
 
 # Labels for each csv file
 labels = [
-  "Label for path1",
-  "Label for path2"
+  "",
 ]
+colors =['red', 'green', '#9A0EEA']
 
-colors =['blue', 'green', '#A2142F']
-
-# draw_one_plot('path')
-draw_multiple(sources, labels, colors)
+draw_one_plot('', colors)
+# draw_multiple(sources, labels)
